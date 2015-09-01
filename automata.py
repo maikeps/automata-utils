dbd496b0fb642062a4e6c257d9d906067752531e
Maikeps <psmaike@gmail.com>
2015-09-01 17:42:56 +0000

    Grammar -> automaton method implemented

--- automata.py
+++ automata.py
@@ -13,6 +13,37 @@ class Automaton:
 
 		self.current_states = [initial_state, ]
 
+	def __str__(self):
+		string = ""
+		# Initial State:
+		string += "Initial State: "
+		string += "\t"+self.initial_state+"\n"
+
+		# Alphabet
+		string += "Alphabet: "
+		string += "\t{"
+		for i in range(len(self.alphabet)-1):
+			string += self.alphabet[i]+", "
+		string += self.alphabet[-1]+"}\n"
+
+		# States
+		string += "States: "
+		string += "\t{"
+		for i in range(len(self.states)-1):
+			if self.states[i] is not 'F' and self.states[i] is not 'M':
+				string += self.states[i]+", "
+		string += self.states[-1]+"}\n"
+
+		# Transition table
+		string += "Transition table:\n"
+		for key in self.transition:
+			for transition in self.transition[key]:
+				if key is not 'M' and key is not 'F':
+					string += "\t("+key+", "+transition+") -> "+self.transition[key][transition][0]+"\n"
+
+
+		return string
+
 	def change_state(self, _input):
 		achieved_states = []
 		deleted_states = []
@@ -33,7 +64,7 @@ class Automaton:
 			if state not in self.current_states:
 				self.current_states.append(state)
 
-		print(self.current_states)
+		# print(self.current_states)
 
 	def check_done(self):
 		for state in self.current_states:
@@ -75,6 +106,16 @@ class Automaton:
 				if final_state in state:
 					new_accept_states.append(state)
 
+		for state in transition:
+			for next_state in transition[state]:
+				if len(transition[state][next_state]) > 1:
+					state_str = ""
+				
+					for aux in transition[state][next_state]:
+						state_str += aux
+					transition[state][next_state] = [state_str]
+
+
 		return Automaton(new_states, self.alphabet,	transition,	self.initial_state, new_accept_states)
 
 	def addState(self, new_state_arr, new_state_name, transition, transition_aux):
@@ -99,6 +140,7 @@ class Automaton:
 			if aux_str not in transition_aux:
 				self.addState(transition_aux[new_state_name][item], aux_str, transition, transition_aux)
 
+
 class Grammar:
 	def __init__(self, nonterminal, terminal, production, start_symbol):
 		self.nonterminal = nonterminal
@@ -106,6 +148,41 @@ class Grammar:
 		self.production = production
 		self.inverse_prod = self.invert_production(production)
 		self.start_symbol = start_symbol
+
+	def __str__(self):
+		string = ""
+
+		# Start symbol
+		string += "Start symbol: "
+		string += "\t"+self.start_symbol+"\n"
+
+		# Terminal symbols
+		string += "Terminal symbols: "
+		string += "\t{"
+		for i in range(len(self.terminal)-1):
+			string += self.terminal[i]+", "
+		string += self.terminal[-1]+"}\n"
+
+		# Non terminal symbols
+		string += "Non terminal symbols: "
+		string += "\t{"
+		for i in range(len(self.nonterminal)-1):
+			string += self.nonterminal[i]+", "
+		string += self.nonterminal[-1]+"}\n"
+
+		# Production rules
+		string += "Production rules: \n"
+		for key in self.production:
+			string += "\t"+key+" -> "+self.production[key][0]
+			for i in range(1, len(self.production[key])-1):
+				# print(self.production[key][i])
+				string += " | "+self.production[key][i]
+			string += " | "+self.production[key][-1]+"\n"
+			# for production in self.production[key]:
+
+				# print(production)
+
+		return string
 
 	def invert_production(self, production):
 		inverse_prod = {}
@@ -164,11 +241,52 @@ class Grammar:
 
 		return word == self.start_symbol
 
+	# Grammar type 3
+	def generate_automaton(self):
+		states = [item for item in self.nonterminal]
+		states.append('F')
+		states.append('M')
+		alphabet = [item for item in self.terminal]
+		initial_state = self.start_symbol
+
+		accept_states = ['F']
+		transition = {}
+		# for key in self.production:
+		# 	for item in self.production[key]:
+		# 		if item.islower():
+		# 			transition[key][item] = ['F']
+					# accept_states.append(key)
+		
+		for key in self.production:
+			transition[key] = {}
+			for item in self.production[key]:
+				if item.islower():
+					try:
+						transition[key][item].append('F')
+					except KeyError:
+						transition[key][item] = ['F']
+				else:
+					char = item
+					next_state = ''
+					for letter in item:
+						if letter.isupper():
+							char = char.replace(letter, '')
+							next_state = letter
+						# else:
+					try:
+						transition[key][char].append(next_state)
+					except KeyError:
+						transition[key][char] = [next_state]
 
 
-	def replace(self, word, symbol, new_symbol):
-		return word.replace(symbol, new_symbol)
+		transition['F'] = {}
+		transition['M'] = {}
+		for char in alphabet:	
+			transition['F'][char] = ['M']
+			transition['M'][char] = ['M']
 
+		automaton = Automaton(states, alphabet, transition, initial_state, accept_states)
+		return automaton.determinize()
 
 # automaton
 states = ['q0', 'q1', 'q2', 'M']
@@ -184,18 +302,18 @@ accept_states = ['q2']
 
 # grammar
 start_symbol = 'S'
-terminal = set(['a', 'b'])
-nonterminal = set(['S', 'A'])
+terminal = ['a', 'b']
+nonterminal = ['S', 'A']
 production = {
-	'S': ['A'],
-	'A': ['aAb', 'ab', 'B'],
-	'B': ['ab']
+	'S': ['aA', 'bS'],
+	'A': ['aS', 'bA', 'a']
 }
 
 automaton = Automaton(states, alphabet, transition, initial_state, accept_states)
 grammar = Grammar(nonterminal, terminal, production, start_symbol)
 
-print(grammar.derive('S', 'aaaaaabbbbbb'))
+print(grammar)
+print(grammar.generate_automaton())
 
 # print(automaton.verify_word('001010101010111000100000001'))
 # print(automaton.determinize().transition)
