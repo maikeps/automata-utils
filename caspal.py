2a6f85a82e5cbc0469683da59e9953edda06f7c0
maikeps <psmaike@gmail.com>
2015-11-05 18:14:49 -0200

    CasPal programming language - first steps

---
+++ caspal.py
@@ -0,0 +1,59 @@
+from automata import RegularExpression as RE
+from automata import Automaton
+
+# PR
+grampro = RE('grampro').generate_automaton()
+rav = RE('rav').generate_automaton()
+ginbe = RE('ginbe').generate_automaton()
+ned = RE('ned').generate_automaton()
+rof = RE('rof').generate_automaton()
+od = RE('od').generate_automaton()
+fi = RE('fi').generate_automaton()
+enth = RE('enth').generate_automaton()
+seel = RE('seel').generate_automaton()
+adre = RE('adre').generate_automaton()
+tewri = RE('tewri').generate_automaton()
+
+# DeclVar
+tegerin = RE('tegerin').generate_automaton()
+leanboo = RE('leanboo').generate_automaton()
+ingstr = RE('ingstr').generate_automaton()
+tegerin_arr = RE('tegerin[]').generate_automaton()
+leanboo_arr = RE('leanboo[]').generate_automaton()
+ingstr_arr = RE('ingstr[]').generate_automaton()
+
+
+# CONST
+etru = RE('etru').generate_automaton()
+sefal = RE('sefal').generate_automaton()
+_ = RE('0').generate_automaton()
+a = RE('1').generate_automaton()
+aa = RE('2').generate_automaton()
+aaa = RE('3').generate_automaton()
+aaaa = RE('4').generate_automaton()
+aaaaa = RE('5').generate_automaton()
+aaaaaa = RE('6').generate_automaton()
+aaaaaaa = RE('7').generate_automaton()
+aaaaaaaa = RE('8').generate_automaton()
+aaaaaaaaa = RE('9').generate_automaton()
+
+# OP
+add = Automaton(['p', 'q', 'M'], ['+'], {'p': {'+': ['q']},'q': {'+': ['p']},'M': {'+': ['M']}}, 'p', ['q'])
+sub = RE('-').generate_automaton()
+div = RE('/').generate_automaton()
+mul = Automaton(['p', 'q', 'M'], ['*'], {'p': {'*': ['q']},'q': {'*': ['p']},'M': {'*': ['M']}}, 'p', ['q'])
+ro = RE('ro').generate_automaton()
+nad = RE('nad').generate_automaton()
+ton = RE('ton').generate_automaton()
+grt = RE('>').generate_automaton()
+lst = RE('<').generate_automaton()
+neq = RE('!=').generate_automaton()
+eq = RE('==').generate_automaton()
+ass = RE('=').generate_automaton()
+tesquo = RE('\'').generate_automaton()
+
+
+pr = grampro | rav | ginbe | ned | rof | od | fi | enth | seel | adre | tewri
+declvar = tegerin | leanboo | ingstr | tegerin_arr | leanboo_arr | ingstr_arr
+const = etru | sefal | _ | a | aa | aaa | aaaa | aaaaa | aaaaaa | aaaaaaa | aaaaaaaa | aaaaaaaaa
+op = add | sub | div | mul | ro | nad | ton | grt | lst | neq | eq | ass | tesquo
