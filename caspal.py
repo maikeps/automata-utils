0f88d474fc0f9eb98bcc4ee9a3d66e28742bd78c
Maikeps <psmaike@gmail.com>
2015-11-06 10:30:58 +0000

    Added separators and identifiers to the language

--- caspal.py
+++ caspal.py
@@ -36,6 +36,7 @@ aaaaaa = RE('6').generate_automaton()
 aaaaaaa = RE('7').generate_automaton()
 aaaaaaaa = RE('8').generate_automaton()
 aaaaaaaaa = RE('9').generate_automaton()
+string = RE('\'(q|w|e|r|t|y|u|i|o|p|a|s|d|f|g|h|j|k|l|z|x|c|v|b|n|m|Q|W|E|R|T|Y|U|I|O|P|A|S|D|F|G|H|J|K|L|Z|X|C|V|B|N|M|1|2|3|4|5|6|7|8|9|0| )*\'')
 
 # OP
 add = Automaton(['p', 'q', 'M'], ['+'], {'p': {'+': ['q']},'q': {'+': ['p']},'M': {'+': ['M']}}, 'p', ['q'])
@@ -50,10 +51,33 @@ lst = RE('<').generate_automaton()
 neq = RE('!=').generate_automaton()
 eq = RE('==').generate_automaton()
 ass = RE('=').generate_automaton()
-tesquo = RE('\'').generate_automaton()
+# tesquo = RE('\'').generate_automaton()
+
+# ID
+di = RE('(q|w|e|r|t|y|u|i|o|p|a|s|d|f|g|h|j|k|l|z|x|c|v|b|n|m|Q|W|E|R|T|Y|U|I|O|P|A|S|D|F|G|H|J|K|L|Z|X|C|V|B|N|M|_)(q|w|e|r|t|y|u|i|o|p|a|s|d|f|g|h|j|k|l|z|x|c|v|b|n|m|Q|W|E|R|T|Y|U|I|O|P|A|S|D|F|G|H|J|K|L|Z|X|C|V|B|N|M|_|1|2|3|4|5|6|7|8|9|0)*').generate_automaton()
+
+# SEP
+space = RE(' ').generate_automaton()
+semicolon = RE(';').generate_automaton()
+dot = RE('.').generate_automaton()
+colon = RE(',').generate_automaton()
 
 
 pr = grampro | rav | ginbe | ned | rof | od | fi | enth | seel | adre | tewri
 declvar = tegerin | leanboo | ingstr | tegerin_arr | leanboo_arr | ingstr_arr
 const = etru | sefal | _ | a | aa | aaa | aaaa | aaaaa | aaaaaa | aaaaaaa | aaaaaaaa | aaaaaaaaa
-op = add | sub | div | mul | ro | nad | ton | grt | lst | neq | eq | ass | tesquo
+op = add | sub | div | mul | ro | nad | ton | grt | lst | neq | eq | ass# | tesquo
+ids = di
+sep = space | semicolon | dot | colon
+
+pr = pr.determinize().beautify().minimize()
+declvar = declvar.determinize().beautify().minimize()
+const = declvar.determinize().beautify().minimize()
+op = op.determinize().beautify().minimize()
+ids = ids.determinize().beautify().minimize()
+sep = sep.determinize().beautify().minimize()
+
+final = pr | declvar | const | op | ids | sep
+
+print(final)
+print(final.analyze('grampro name;'))
